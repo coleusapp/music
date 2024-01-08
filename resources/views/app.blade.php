0fc3957ef98534ecc88aa2dfd6cbbeaf4b99ce9a
Amirmasoud <amirmasood33@gmail.com>
2024-01-08 00:15:15 -0700

    wip

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
     @routes
-    @vite('resources/js/app.js', 'dist/coleus/account')
+    @vite('resources/js/app.js', 'dist/coleus/music')
     @inertiaHead
 </head>
 <body>
