b4740598b1aa1991a3b027b197bc86b52a2c54f5
Amirmasoud <amirmasood33@gmail.com>
2025-12-01 19:13:11 -0700

    wip

--- resources/js/layouts/MusicLayout.vue
+++ resources/js/layouts/MusicLayout.vue
@@ -6,7 +6,7 @@ import Aside from '@/components/Aside.vue';
 <template>
     <AppLayout>
         <div class="flex w-full flex-col-reverse md:flex-row">
-            <Aside />
+            <!-- <Aside /> -->
             <div class="w-full h-full p-4 overflow-scroll">
                 <div class="w-full max-w-7xl mx-auto ">
                     <slot />
